04973528020046727a054fe65ae4748926212c34
SYeameen <67371564+SYeameen@users.noreply.github.com>
2022-04-25 23:06:37 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Guid } from 'guid-typescript';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo_list';
+  todos: Todo[] = [];
+
+  onSubmit(form: NgForm) {
+    let todo = new Todo(Guid.create(), form.value.title, false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+
+  onClick(id: Guid) {
+    let todo = this.todos.filter((x) => x.id === id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid) {
+    let todo = this.todos.filter((x) => x.id === id)[0];
+    let index = this.todos.indexOf(todo, 0);
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
